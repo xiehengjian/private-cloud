e5939247a5e585d8c50b45e05aacca00c20a6544
xiehengjian <846188037@qq.com>
2022-10-02 18:54:33 +0000

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,16 @@
 # 检查网络环境
 cd basic && sh install.sh && cd ..
-cd sshd && sh install.sh && cd ..
+
+
+ping -c 1 "google.com"
+if [ $? -eq 0 ]; then
+    echo "使用国际网络"
+	export region="i18n"
+else
+    echo "使用国内网络"
+    export region="cn"
+fi
+
 cd docker && sh install.sh && cd ..
 cd  kubectl && sh install.sh && cd ..
 cd rke && sh install.sh && cd ..
