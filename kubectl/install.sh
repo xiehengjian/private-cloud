c177df8c8a89bf5f466ff56c162aedb032b02cd8
xiehengjian <846188037@qq.com>
2022-10-02 15:48:14 +0000

    fix

--- kubectl/install.sh
+++ kubectl/install.sh
@@ -8,10 +8,10 @@ fi
 ping -c 1 "google.com"
 if [ $? -eq 0 ]; then
     echo "使用国际网络"
-	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && cp kubectl /usr/bin/
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && sudo chmod +x kubectl && sudo cp kubectl /usr/bin/
 else
     echo "使用国内网络"
-	curl -fL "https://kinshy-generic.pkg.coding.net/south/study/kubectl?version=latest" -o kubectl && chmod +x kubectl && cp kubectl /usr/bin/
+	curl -fL "https://kinshy-generic.pkg.coding.net/south/study/kubectl?version=latest" -o kubectl && sudo chmod +x kubectl && sudo cp kubectl /usr/bin/
 fi
 
 
