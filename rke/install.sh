4e8d0270a429a8409405c753b4f3a4b29734a287
xiehengjian <846188037@qq.com>
2022-10-03 08:38:44 +0000

    fix

--- rke/install.sh
+++ rke/install.sh
@@ -7,14 +7,14 @@ if type "rke" >/dev/null 2>&1;then
 fi
 
 
-ping -c 1 "google.com"
-if [ $? -eq 0 ]; then
-    echo "使用国际网络"
-	curl -LO "https://github.com/rancher/rke/releases/download/v1.3.10/rke_linux-amd64" && mv rke_linux-amd64 rke && sudo chmod +x rke && ./rke --version && sudo cp rke /usr/bin/
-else
-    echo "使用国内网络"
-	curl -fL "https://kinshy-generic.pkg.coding.net/south/study/rke?version=latest" -o rke && sudo chmod +x rke && ./rke --version && sudo cp rke /usr/bin/
-fi
+# ping -c 1 "google.com"
+# if [ $? -eq 0 ]; then
+#     echo "使用国际网络"
+curl -LO "https://github.com/rancher/rke/releases/download/v1.3.15/rke_linux-amd64" && mv rke_linux-amd64 rke && sudo chmod +x rke && ./rke --version && sudo cp rke /usr/bin/
+# else
+#     echo "使用国内网络"
+# 	curl -fL "https://kinshy-generic.pkg.coding.net/south/study/rke?version=v1.3.15" -o rke && sudo chmod +x rke && ./rke --version && sudo cp rke /usr/bin/
+# fi
 
 ssh-keygen
 ssh-copy-id heng@127.0.0.1
